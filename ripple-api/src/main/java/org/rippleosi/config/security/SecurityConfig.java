71904640c6c677a1a9c38fc4636961963f4e83b5
Josh.Hilland <josh.hilland@answerconsulting.com>
2016-05-18 10:09:05 +0100

    Implemented initial call to the ID server to redirect to the login page and retrieve the auth tokens

--- ripple-api/src/main/java/org/rippleosi/config/security/SecurityConfig.java
+++ ripple-api/src/main/java/org/rippleosi/config/security/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,22 +19,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private String authenticationServerUrl;
 
     @Value("${authentication.client.id}")
-    private String applicationId;
+    private String clientId;
+
+    @Value("${authentication.redirect.uri}")
+    private String redirectUri;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
-            .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
                 .and()
-            .formLogin()
-                .loginPage(authenticationServerUrl + "/login?signin=" + applicationId)
+            .authorizeRequests()
+                .antMatchers("/api/swagger-ui.html").permitAll()
+                .antMatchers("/api/token").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/**").authenticated()
+                .and()
+            .openidLogin()
+                .loginPage(authenticationServerUrl())
                 .and()
             .csrf()
-			    .csrfTokenRepository(csrfTokenRepository())
+                .csrfTokenRepository(csrfTokenRepository())
                     .and()
-			    .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
         // @formatter:on
     }
 
@@ -41,5 +51,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         repository.setHeaderName("X-XSRF-TOKEN");
 
         return repository;
+    }
+
+    private String authenticationServerUrl() {
+        final UriComponentsBuilder builder = UriComponentsBuilder
+            .fromHttpUrl(authenticationServerUrl + "/identity/connect/authorize")
+            .queryParam("client_id", clientId)
+            .queryParam("scope", "openid profile email api")
+            .queryParam("response_type", "id_token token")
+            .queryParam("redirect_uri", redirectUri)
+            .queryParam("nonce", "nonce");
+
+        return builder.toUriString();
     }
 }
