ebf9e07ed8ab0031672652055dcd6e672adb441e
Josh.Hilland <josh.hilland@answerconsulting.com>
2016-05-23 09:08:51 +0100

    Added copyrights to all new security based files

--- ripple-api/src/main/java/org/rippleosi/config/security/SecurityConfig.java
+++ ripple-api/src/main/java/org/rippleosi/config/security/SecurityConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Ripple OSI
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.rippleosi.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -6,10 +21,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
